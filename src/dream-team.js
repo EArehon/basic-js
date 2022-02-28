de99760cd2c43fb489461d9f09ae06cb9f0cffd0
EArehon <earehon@gmail.com>
2022-02-28 12:42:18 +0300

    feat: add what-season solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(arr) {
+  if (!Array.isArray(arr)) return false;
+  
+  let res = '';
+  arr.filter(el => typeof el == 'string').forEach(el => res += el.trim()[0]);
+
+  return res.toUpperCase().split('').sort().join('');
 }
 
 module.exports = {
